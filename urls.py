09d593ff4398cc3e858d874465392a3363871eaf
Jack Hsu <jhsu@odm-jhsu.local>
2011-08-08 16:28:33 -0400

    Updated templates

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
+from django.conf import settings
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += staticfiles_urlpatterns()  
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()  
