7672f8b163963378db9d6b8cae6b391d0a88515f
Jack Hsu <jack.hsu@oicr.on.ca>
2011-08-09 14:38:55 -0400

    Optimize for different browser dimensions

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,19 @@
 from core.models import Settings
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages.admin import FlatPageAdmin
 
 class SettingsAdmin(admin.ModelAdmin): 
     class Media: 
-        js = ('/static/tiny_mce/tiny_mce.js', '/static/filebrowser/js/TinyMCEAdmin.js',) 
+        js = ('%s/tiny_mce/tiny_mce.js' % settings.STATIC_URL, '%s/filebrowser/js/TinyMCEAdmin.js' % settings.STATIC_URL,) 
 
 admin.site.register(Settings, SettingsAdmin)
+
+class FlatpageAdmin(FlatPageAdmin): 
+    class Media: 
+        js = ('%s/tiny_mce/tiny_mce.js' % settings.STATIC_URL, '%s/filebrowser/js/TinyMCEAdmin.js' % settings.STATIC_URL,) 
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatpageAdmin)
+
