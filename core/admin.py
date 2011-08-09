ef7dbf8a3335f01b516c70a73745e30758b0bb89
Jack Hsu <jack.hsu@oicr.on.ca>
2011-08-09 12:15:58 -0400

    Added tinymce

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,8 @@
 from core.models import Settings
 from django.contrib import admin
 
-admin.site.register(Settings)
+class SettingsAdmin(admin.ModelAdmin): 
+    class Media: 
+        js = ('/static/tiny_mce/tiny_mce.js', '/static/filebrowser/js/TinyMCEAdmin.js',) 
+
+admin.site.register(Settings, SettingsAdmin)
--- settings.py
+++ settings.py
@@ -110,6 +110,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
     'context_processors.domain',
     'context_processors.title_and_tagline',
 )
