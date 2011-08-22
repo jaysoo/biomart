0d1fe224c4eea3453ebb4507d0670f935142d2a0
Jack Hsu <jack.hsu@oicr.on.ca>
2011-08-22 10:22:33 -0400

    Adjusted grappelli min-width styles.

--- site_static/js/TinyMCEAdmin.js
+++ site_static/js/TinyMCEAdmin.js
@@ -34,7 +34,7 @@ tinyMCE.init({
     theme_advanced_toolbar_location: "top",
     theme_advanced_toolbar_align: "left",
     theme_advanced_statusbar_location: "none",
-    theme_advanced_buttons1: "formatselect,bold,italic,underline,bullist,numlist,blockquote,code,undo,redo,link,unlink,image,code,fullscreen,pasteword,media,charmap",
+    theme_advanced_buttons1: "formatselect,bold,italic,underline,bullist,numlist,|,undo,redo,link,unlink,image,code,fullscreen,pasteword,media,charmap,code",
     theme_advanced_buttons2: "",
     theme_advanced_buttons3: "",
     theme_advanced_path: false,
