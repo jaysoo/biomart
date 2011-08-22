19e856630d9652cdf4104b81cd86c24a54879991
Jack Hsu <jack.hsu@oicr.on.ca>
2011-08-22 10:10:28 -0400

    Added <pre> block control to admin.

--- core/admin.py
+++ core/admin.py
@@ -28,13 +28,13 @@ admin.site.register(ThirdPartySoftware, ThirdPartySoftwareAdmin)
 
 class SettingsAdmin(admin.ModelAdmin): 
     class Media: 
-        js = ('%s/tiny_mce/tiny_mce.js' % settings.STATIC_URL, '%s/filebrowser/js/TinyMCEAdmin.js' % settings.STATIC_URL,) 
+        js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL, '%sjs/TinyMCEAdmin.js' % settings.STATIC_URL,) 
 
 admin.site.register(Settings, SettingsAdmin)
 
 class FlatpageAdmin(FlatPageAdmin): 
     class Media: 
-        js = ('%s/tiny_mce/tiny_mce.js' % settings.STATIC_URL, '%s/filebrowser/js/TinyMCEAdmin.js' % settings.STATIC_URL,) 
+        js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL, '%sjs/TinyMCEAdmin.js' % settings.STATIC_URL,) 
 
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatpageAdmin)
