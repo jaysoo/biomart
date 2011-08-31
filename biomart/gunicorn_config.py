ce168c8583432556f39f75ab05a60dd2ea56ad93
jhsu <jhsu@550fb8ff-79aa-4783-b0d6-b241222edaee>
2011-08-31 20:01:39 +0000

    Updated settings
    git-svn-id: https://code.oicr.on.ca/svn/dcc_dev/biomart_django/trunk@3749 550fb8ff-79aa-4783-b0d6-b241222edaee

--- biomart/gunicorn_config.py
+++ biomart/gunicorn_config.py
@@ -2,10 +2,3 @@ import multiprocessing
 
 bind = '127.0.0.1:9997'
 workers = multiprocessing.cpu_count() * 2 + 1
-daemon = True
-pidfile = 'gunicorn.pid'
-logfile = 'gunicorn.log'
-preload_app = True
-
-# Minizing impact of any potential memory leaks
-max_requests = 1000
